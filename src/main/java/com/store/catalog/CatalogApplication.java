9ae524e90648af357e8cfdc90d8806b67780824b
Mariane Muniz <mariane.muniz.ribeiro@hotmail.com>
2019-12-24 10:47:38 -0300

    Swagger config - configurated

--- src/main/java/com/store/catalog/CatalogApplication.java
+++ src/main/java/com/store/catalog/CatalogApplication.java
@@ -3,13 +3,18 @@ package com.store.catalog;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Import;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import springfox.documentation.spring.data.rest.configuration.SpringDataRestConfiguration;
+import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
 
 @SpringBootApplication
 @EnableJpaAuditing
 @EnableJpaRepositories("com.store.catalog.repositories")
 @EntityScan("com.store.catalog.models")
+@EnableSwagger2WebMvc
+@Import(SpringDataRestConfiguration.class)
 public class CatalogApplication {
 
 	public static void main(String[] args) {
